968d2d88ff25cce7a411c19ef18015c0dbf6e0c2
BiaMatsushita <beatriz.matsushita@sbuild.com.br>
2021-03-19 17:12:26 -0300

    Tentativa falha de correcao
    Falhamos longe

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\AplicacoesMobile\image_recognition_app-master\image_recognition_app-master"
+export "FLUTTER_APPLICATION_PATH=C:\Users\BeatrizMatsushita\OneDrive - S. Build\Documents\GitHub\POC-Flutter-Funcao-OCR"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
